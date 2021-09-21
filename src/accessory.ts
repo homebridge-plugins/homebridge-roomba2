homebridge-plugins/homebridge-roomba2

commit 9dd26c2968f32bffacff561be5ab4e7e1204ab9f

Fix docked state

src/accessory.ts
@@ -9,7 +9,7 @@ const OLD_STATUS = "oldStatus";
 interface Status {
     error: null
     running: 0 | 1
-    charging: 0 | 1
+    charging: number
     batteryLevel: string | number
     batteryStatus: string | 0 | 1
     binFull: boolean
@@ -302,7 +302,7 @@ export default class RoombaAccessory implements AccessoryPlugin {
             if (error) {
                 callback(error);
             } else {
-                callback(null, !status!.charging);
+                callback(null, status!.charging === this.api.hap.Characteristic.ChargingState.CHARGING);
             }
         });
     }

commit 68be30fce33685ffcd25de4da7bb629f8c2ebcac

Fix docked sensor to be closed when docked
Regression from 9dd26c2968f32bffacff561be5ab4e7e1204ab9f

src/accessory.ts
@@ -484,8 +484,8 @@ export default class RoombaAccessory implements AccessoryPlugin {
         ? this.api.hap.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
         : this.api.hap.Characteristic.ContactSensorState.CONTACT_DETECTED;
     private dockedStatus = (status: Status) => status.charging
-        ? this.api.hap.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
-        : this.api.hap.Characteristic.ContactSensorState.CONTACT_DETECTED;
+        ? this.api.hap.Characteristic.ContactSensorState.CONTACT_DETECTED
+        : this.api.hap.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
     private batteryLevelStatus = (status: Status) => status.batteryLevel;
     private binStatus = (status: Status) => status.binFull
         ? this.api.hap.Characteristic.FilterChangeIndication.CHANGE_FILTER