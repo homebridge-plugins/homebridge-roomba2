5cbcf6a9c0c354a4a192d2c4cd58c695779cac1c
Karl von Randow <karl@xk72.com>
2021-10-27 09:49:38 +1300

    Refactor dockOnStop config option
    I think it's clearer as a dropdown

--- src/accessory.ts
+++ src/accessory.ts
@@ -80,7 +80,7 @@ export default class RoombaAccessory implements AccessoryPlugin {
     private robotpwd: string
     private ipaddress: string
     private firmware: string
-    private dockOnStop: boolean
+    private stopBehaviour: "home" | "pause"
 
     private accessoryInfo: Service
     private filterMaintenance: Service
@@ -133,7 +133,7 @@ export default class RoombaAccessory implements AccessoryPlugin {
         this.robotpwd = config.robotpwd;
         this.ipaddress = config.ipaddress;
         this.firmware = "N/A";
-        this.dockOnStop = config.dockOnStop !== undefined ? config.dockOnStop : true;
+        this.stopBehaviour = config.stopBehaviour !== undefined ? config.stopBehaviour : "home";
 
         const showDockAsContactSensor = config.dockContactSensor === undefined ? true : config.dockContactSensor;
         const showRunningAsContactSensor = config.runningContactSensor;
@@ -476,7 +476,7 @@ export default class RoombaAccessory implements AccessoryPlugin {
                         /* Force a refresh of state so we pick up the new state quickly */
                         this.refreshState();
                     
-                        if (this.dockOnStop) {
+                        if (this.stopBehaviour === "home") {
                             this.log("Roomba paused, returning to Dock");
                             await this.dockWhenStopped(roomba, 3000);
                         } else {
