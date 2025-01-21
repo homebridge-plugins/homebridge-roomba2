837949ba50078a9019c19daf5d519d153f611144
Karl von Randow <karl@xk72.com>
2025-01-21 21:34:26 +1300

    chore: convert to esm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import type { API } from 'homebridge'
 
-import RoombaAccessory from './accessory'
-import { ACCESSORY_NAME, PLUGIN_NAME } from './settings'
+import RoombaAccessory from './accessory.js'
+import { ACCESSORY_NAME, PLUGIN_NAME } from './settings.js'
 
 /**
  * This method registers the platform with Homebridge
  */
-export = (api: API): void => {
+export default function (api: API): void {
   api.registerAccessory(PLUGIN_NAME, ACCESSORY_NAME, RoombaAccessory)
 }
