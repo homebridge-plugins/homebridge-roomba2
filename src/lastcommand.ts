939703de8617d2a76723d802d944dcb2f13a31ab
Donavan Becker <beckersmarthome@icloud.com>
2025-01-23 07:30:42 -0600

    v1.5.1 (#149)
    ## [1.5.1](https://github.com/homebridge-plugins/homebridge-roomba2/releases/tag/v1.5.1) (2025-01-23)
    
    ### What's Changes
    - Fixed Cannot find module Issue [#148](https://github.com/homebridge-plugins/homebridge-roomba2/issues/148)
    - Housekeeping and updated dependencies.
    
    **Full Changelog**: https://github.com/homebridge-plugins/homebridge-roomba2/compare/v1.5.0...v1.5.1

--- src/lastcommand.ts
+++ src/lastcommand.ts
@@ -1,21 +1,23 @@
-var dorita980 = require('dorita980');
+const dorita980 = require("dorita980");
 
 if (!process.argv[4]) {
-  console.log('Usage: npm run getlastcommand <robot_blid> <robot_pwd> <robot_ip_address>');
-  process.exit();
+    console.log("Usage: npm run getlastcommand <robot_blid> <robot_pwd> <robot_ip_address>");
+    process.exit();
 }
 
 const robot_blid = process.argv[2];
 const robot_pwd = process.argv[3];
 const robot_ip_address = process.argv[4];
 
-var myRobotViaLocal = new dorita980.Local(robot_blid, robot_pwd, robot_ip_address);
+const myRobotViaLocal = new dorita980.Local(robot_blid, robot_pwd, robot_ip_address);
 
-myRobotViaLocal.on('connect', init);
+myRobotViaLocal.on("connect", init);
 
-function init () {
+function init() {
 
-  myRobotViaLocal.getRobotState(['lastCommand'])
-  .then((result: any) => {console.log("lastCommand:", result.lastCommand, ", regionsDetails:", result.lastCommand.regions); myRobotViaLocal.end()})
-  .catch(console.log);
+    myRobotViaLocal.getRobotState(["lastCommand"])
+        .then((result: { lastCommand: { regions: { regionId: number; regionType: string }[] } }) => {
+            console.log("lastCommand:", result.lastCommand, ", regionsDetails:", result.lastCommand.regions); myRobotViaLocal.end(); 
+        })
+        .catch(console.log);
 }
