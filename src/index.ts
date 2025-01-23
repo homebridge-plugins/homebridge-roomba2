a20971a41fffdc62cdcff8f5191ee57f148928c0
Donavan Becker <beckersmarthome@icloud.com>
2025-01-22 22:05:00 -0600

    v1.5.1
    - Fixed Cannot find module Issue [#148](https://github.com/homebridge-plugins/homebridge-roomba2/issues/148)
    - Housekeeping and updated dependencies.
    
    **Full Changelog**: https://github.com/homebridge-plugins/homebridge-roomba2/compare/v1.5.0...v1.5.1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { API } from "homebridge";
 
-import { ACCESSORY_NAME, PLUGIN_NAME } from "./settings";
-import RoombaAccessory from "./accessory";
+import { ACCESSORY_NAME, PLUGIN_NAME } from "./settings.js";
+import RoombaAccessory from "./accessory.js";
 
 /**
  * This method registers the platform with Homebridge
