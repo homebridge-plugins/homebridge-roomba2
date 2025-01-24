249f487a3ac1bc19885b9a406b4f3f1778bae563
Karl von Randow <karl@xk72.com>
2025-01-25 11:09:13 +1300

    chore: remove defunct ACCESSORY_NAME setting

--- src/settings.ts
+++ src/settings.ts
@@ -7,9 +7,3 @@ export const PLATFORM_NAME = 'Roomba2'
  * This must match the name of your plugin as defined the package.json
  */
 export const PLUGIN_NAME = '@homebridge-plugins/homebridge-roomba2'
-
-/**
- * This is the name of the accessory that users will use to register the plugin in the Homebridge config.json
- * This is also the name of the accessory that will be displayed in the Home app
- */
-export const ACCESSORY_NAME = 'Roomba2'
