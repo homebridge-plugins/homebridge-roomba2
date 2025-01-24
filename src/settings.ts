7075903d6ab9ef7acaab0500bf425e0b498af985
Karl von Randow <karl@xk72.com>
2025-01-25 11:08:31 +1300

    fix: update PLUGIN_NAME to match new scoped plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -6,7 +6,7 @@ export const PLATFORM_NAME = 'Roomba2'
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-roomba2'
+export const PLUGIN_NAME = '@homebridge-plugins/homebridge-roomba2'
 
 /**
  * This is the name of the accessory that users will use to register the plugin in the Homebridge config.json
