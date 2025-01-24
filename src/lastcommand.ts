c3540c49d2a6f27a37ad54ef21c492130dbc1230
Donavan Becker <beckersmarthome@icloud.com>
2025-01-24 00:35:21 -0600

    fix: linting

--- src/lastcommand.ts
+++ src/lastcommand.ts
@@ -1,23 +1,28 @@
-const dorita980 = require("dorita980");
+/* eslint-disable no-console */
+import process from 'node:process'
+
+import dorita980 from 'dorita980'
 
 if (!process.argv[4]) {
-    console.log("Usage: npm run getlastcommand <robot_blid> <robot_pwd> <robot_ip_address>");
-    process.exit();
+  console.log('Usage: npm run getlastcommand <robot_blid> <robot_pwd> <robot_ip_address>')
+  process.exit()
 }
 
-const robot_blid = process.argv[2];
-const robot_pwd = process.argv[3];
-const robot_ip_address = process.argv[4];
+const robot_blid = process.argv[2]
+const robot_pwd = process.argv[3]
+const robot_ip_address = process.argv[4]
 
-const myRobotViaLocal = new dorita980.Local(robot_blid, robot_pwd, robot_ip_address);
+const myRobotViaLocal = new dorita980.Local(robot_blid, robot_pwd, robot_ip_address)
 
-myRobotViaLocal.on("connect", init);
+myRobotViaLocal.on('connect', init)
+
+type RobotState = dorita980.RobotState & { lastCommand?: { regions: { regionId: number, regionType: string }[] } }
 
 function init() {
-
-    myRobotViaLocal.getRobotState(["lastCommand"])
-        .then((result: { lastCommand: { regions: { regionId: number; regionType: string }[] } }) => {
-            console.log("lastCommand:", result.lastCommand, ", regionsDetails:", result.lastCommand.regions); myRobotViaLocal.end(); 
-        })
-        .catch(console.log);
+  myRobotViaLocal.getRobotState(['lastCommand'])
+    .then((result: RobotState) => {
+      console.log('lastCommand:', result.lastCommand, ', regionsDetails:', result.lastCommand?.regions)
+      myRobotViaLocal.end()
+    })
+    .catch(console.log)
 }
